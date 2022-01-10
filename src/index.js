67b786dbd758fb524ea42841f0505fdd604220e1
Valery <valerykarnilayeu@gmail.com>
2022-01-10 19:05:58 +0300

    feat: add task logik

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let array = "" + n;
+  array = array.split('');
+  if (array[0] == "-") {
+    array.shift();
+    return +(array.reverse().join(""));
+  } return +(array.reverse().join(""));
 }
